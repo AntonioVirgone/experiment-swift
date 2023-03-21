3523af707c970bd407b6ff4449604ba0d3d8eb59
antonio.virgone <antonio.virgone@wisemotions.com>
2023-03-21 08:28:57 +0100

    Initial Commit

---
+++ experiment/experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  experimentApp.swift
+//  experiment
+//
+//  Created by Antonio Virgone on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
